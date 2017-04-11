c7bef2b6b3aab631fe8077302ab5a8ce48894f57
Kaczucha69 <cieciumaly@gmail.com>
2017-04-11 17:08:28 +0200

    Filer reader

--- app/src/main/java/com/example/kaczor/tmc_shpreader/Shape/ShapeFragment.java
+++ app/src/main/java/com/example/kaczor/tmc_shpreader/Shape/ShapeFragment.java
@@ -6,15 +6,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.kaczor.tmc_shpreader.Others.FileChooser;
 import com.example.kaczor.tmc_shpreader.R;
+
+import java.io.File;
 
 
 public class ShapeFragment extends Fragment{
 
+    private View view;
+    private FileChooser fileChooser;
+    private Button load;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.shapefragment, container, false);
+        view = inflater.inflate(R.layout.shapefragment, container, false);
+
+        load = (Button) view.findViewById(R.id.load);
+        load.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fileChooser = new FileChooser(getActivity());
+                fileChooser.setFileListener(new FileChooser.FileSelectedListener() {
+                    @Override
+                    public void fileSelected(File file) {
+
+                    }
+                }).showDialog();
+            }
+        });
+
+
+
+        return view;
     }
 }
